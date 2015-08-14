c4ece6a51a19e51fd8e8de1180c0114caedc5503
Runxuan Shu <shurunxuan@hotmail.com>
2015-08-14 14:08:02 -0700

    Setting File added

--- Rythmium/GameViewController.swift
+++ Rythmium/GameViewController.swift
@@ -45,10 +45,15 @@ var artViewBackground: MPMediaItemArtwork?
 
 var Left = [Double]()
 
-class GameViewController: UIViewController {
+var settingFile = FileClass()
 
+var settings: [String : String] = [:]
+
+
+class GameViewController: UIViewController {
+    
     override func viewDidLoad() {
-       
+        
         super.viewDidLoad()
         
         // 使exporter显示
@@ -74,6 +79,39 @@ class GameViewController: UIViewController {
             child.zPosition = -1000
         }
         
+        // 读取设置
+        if !FileClass.isExist("setting.cfg") {
+            settingFile.CreateFile("setting.cfg")
+            settingFile.Write("Visualization\tSpectrum\n")
+        }
+        settingFile.OpenFile("setting.cfg")
+        let settingStr = settingFile.Read()
+        let settingStrs = settingStr.componentsSeparatedByString("\n")
+        for set in settingStrs {
+            if set != "" {
+                let detail = set.componentsSeparatedByString("\t")
+                let content = detail[0]
+                let value = detail[1]
+                settings[content] = value
+                switch content {
+                case "Visualization" :
+                    switch value {
+                    case "Spectrum" :
+                        visualizationType = visualization.Spectrum
+                    case "None" :
+                        visualizationType = visualization.None
+                    default :
+                        NSLog("ERROR, type Visualization setting")
+                        break
+                    }
+                default :
+                    break
+                }
+            }
+        }
+        
+        
+        
         /* Set the scale mode to scale to fit the window */
         Scene.scaleMode = .AspectFill
         
@@ -87,14 +125,14 @@ class GameViewController: UIViewController {
         skView.presentScene(Scene)
         
         
-
+        
     }
     
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return .AllButUpsideDown
@@ -102,12 +140,12 @@ class GameViewController: UIViewController {
             return .All
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
